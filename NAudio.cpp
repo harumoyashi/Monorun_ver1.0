55e330aabe1a45fe83c22b1dd3d34b7ab26f6408
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-10-18 23:07:43 +0900

    オーディオ追加(使えるか知らん)

---
+++ NAudio.cpp
@@ -0,0 +1,148 @@
+#include "NAudio.h"
+
+#include <assert.h>
+
+//チャンクヘッダー
+struct ChunkHeader
+{
+	char id[4];		//チャンク毎のID
+	int32_t size;	//チャンクサイズ
+};
+
+//RIFFヘッダーチャンク
+struct RiffHeader
+{
+	ChunkHeader chunk;	//"RIFF"
+	char type[4];		//"WAVE"
+};
+
+//FMTチャンク
+struct FormatChunk
+{
+	ChunkHeader chunk;	//"fmt"
+	WAVEFORMATEX fmt;	//波形フォーマット
+};
+
+void NAudio::Initialize()
+{
+	HRESULT result;
+
+	CreateInstance();
+	//マスターボイスを生成
+	result = xAudio2->CreateMasteringVoice(&masterVoice);
+}
+
+void NAudio::CreateInstance()
+{
+	HRESULT result;
+
+	result = XAudio2Create(&xAudio2, 0, XAUDIO2_ANY_PROCESSOR);
+}
+
+SoundData NAudio::SoundLoadWave(const char* filename)
+{
+	HRESULT result;
+
+	FileOpen(filename);
+	LoadWave();
+	SetSoundData();
+
+	return soundData;
+}
+
+void NAudio::SoundUnload(SoundData* soundData)
+{
+	//バッファのメモリを解放
+	delete[] soundData->pBuffer;
+
+	soundData->pBuffer = 0;
+	soundData->bufferSize = 0;
+	soundData->wfex = {};
+}
+
+void NAudio::SoundPlayWave(IXAudio2* xAudio2, const SoundData& soundData)
+{
+	HRESULT result;
+
+	//波形フォーマットをもとにSourceVoiceの生成
+	IXAudio2SourceVoice* pSourceVoice = nullptr;
+	result = xAudio2->CreateSourceVoice(&pSourceVoice, &soundData.wfex);
+	assert(SUCCEEDED(result));
+
+	//再生する波形データの設定
+	XAUDIO2_BUFFER buf{};
+	buf.pAudioData = soundData.pBuffer;
+	buf.AudioBytes = soundData.bufferSize;
+	buf.Flags = XAUDIO2_END_OF_STREAM;
+
+	//波形データの再生
+	result = pSourceVoice->SubmitSourceBuffer(&buf);
+	result = pSourceVoice->Start();
+}
+
+void NAudio::FileOpen(const char* filename)
+{
+	//.wavファイルをバイナリモードで開く
+	file.open(filename, std::ios_base::binary);
+	//ファイルオープン失敗を検出する
+	assert(file.is_open());
+}
+
+void NAudio::LoadWave()
+{
+	//RIFFヘッダーの読み込み
+	RiffHeader riff;
+	file.read((char*)&riff, sizeof(riff));
+	//ファイルがRIFFかチェック
+	if (strncmp(riff.chunk.id, "RIFF", 4) != 0)
+	{
+		assert(0);
+	}
+	//タイプがWAVEかチェック
+	if (strncmp(riff.type, "WAVE", 4) != 0)
+	{
+		assert(0);
+	}
+
+	//Formatチャンクの読み込み
+	//チャンクヘッダーの確認
+	file.read((char*)&format, sizeof(ChunkHeader));
+	if (strncmp(format.chunk.id, "fmt", 4) != 0)
+	{
+		assert(0);
+	}
+
+	//チャンク本体の読み込み
+	assert(format.chunk.size <= sizeof(format.fmt));
+	file.read((char*)&format.fmt, format.chunk.size);
+
+	//Dataチャンクの読み込み
+	file.read((char*)&data, sizeof(data));
+	//JUNKチャンクを検出した場合
+	if (strncmp(data.id, "JUNK", 4) != 0)
+	{
+		//読み取り位置をJUNK(ダミー)チャンクの終わりまで進める
+		file.seekg(data.size, std::ios_base::cur);
+		//再読み込み
+		file.read((char*)&data, sizeof(data));
+	}
+
+	if (strncmp(data.id, "data", 4) != 0)
+	{
+		assert(0);
+	}
+
+	//Dataチャンクのデータ部(波形データ)の読み込み
+	pBuffer = new char[data.size];
+	file.read(pBuffer, data.size);
+
+	//Waveファイルを閉じる
+	file.close();
+}
+
+void NAudio::SetSoundData()
+{
+	soundData.wfex = format.fmt;
+	soundData.pBuffer = reinterpret_cast<BYTE*>(pBuffer);
+	soundData.bufferSize = data.size;
+}
---
+++ NAudio.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <xaudio2.h>
+
+#pragma comment(lib,"xaudio2.lib")
+
+#include <fstream>
+#include <wrl.h>
+
+//音声データ
+struct SoundData
+{
+	//波形フォーマット
+	WAVEFORMATEX wfex;
+	//バッファの先頭アドレス
+	BYTE* pBuffer;
+	//バッファのサイズ
+	unsigned int bufferSize;
+};
+
+class NAudio
+{
+private:
+
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+	ComPtr<IXAudio2> xAudio2;
+	IXAudio2MasteringVoice* masterVoice;
+
+
+	std::ifstream file;			//ファイル入力ストリームのインスタンス
+	FormatChunk format = {};	//フォーマットチャンク
+	ChunkHeader data;			//データチャンク
+	char* pBuffer;
+	SoundData soundData = {};	//音声データ
+
+public:
+	void Initialize();
+
+	//XAudioエンジンのインスタンスを生成
+	void CreateInstance();
+	//音声データの読み込み
+	SoundData SoundLoadWave(const char* filename);
+	//音声データ解放
+	void SoundUnload(SoundData* soundData);
+	//音声再生
+	void SoundPlayWave(IXAudio2* xAudio2, const SoundData& soundData);
+
+private:
+	//ファイル読み込み
+	void FileOpen(const char* filename);
+	//.wavデータ読み込み
+	void LoadWave();
+	//読み込んだ音声データを返すために格納
+	void SetSoundData();
+};
